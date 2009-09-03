d8a355f20a2c371725026095a3ef919658439514
Adam Bair <adambair@gmail.com>
2009-09-02 22:18:37 -0400

    fixing a bunch of initially broken things

--- default.rb
+++ default.rb
@@ -46,6 +46,14 @@ class User < ActiveRecord::Base
 end
 CODE
 
+file 'app/helpers/application_helper.rb', <<-CODE
+module ApplicationHelper
+  def clearfix
+    "<div class='clearfix'></div>"
+  end
+end
+CODE
+
 file 'app/controllers/user_sessions_controller.rb', <<-CODE
 class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
@@ -164,7 +172,6 @@ file 'app/views/users/new.html.erb', <<-CODE
 
 <br/>
 <div class='signup_login_normal'>
-  <h2>Register </h2>
   <% form_for @user, :url => account_path do |f| %>
     <%= render :partial => "form", :object => f %>
     <br/>
@@ -186,15 +193,12 @@ file 'app/views/users/edit.html.erb', <<-CODE
 CODE
 
 file 'app/views/users/_form.erb', <<-CODE
-<%= form.label :login, "Login" %>
-<%= form.text_field :login %>
-
 <%= form.label :email, "E-Mail" %>
 <%= form.text_field :email %>
-
+<br/>
 <%= form.label :password, form.object.new_record? ? nil : "Change password" %>
 <%= form.password_field :password %>
-
+<br/>
 <%= form.label :password_confirmation, "Password Confirmation" %>
 <%= form.password_field :password_confirmation %>
 CODE
@@ -202,6 +206,73 @@ CODE
 file 'app/views/users/show.html.erb', <<-CODE
 <h1><%= @user.email %></h1>
 <%= link_to 'Edit my account', edit_user_path(@user) %>
+CODE
+
+file 'app/views/layouts/application.html.erb', <<-CODE
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta http-equiv="content-language" content="en">
+    <link rel="shortcut icon" href="/favicon.ico" />
+
+    <title>Change Me</title>
+
+		<%= stylesheet_link_merged :base %>
+    <%= javascript_include_merged :base %>
+    
+  </head>
+  <body id="<%= controller.controller_name %>_<%= controller.action_name %>">
+    <%= render :partial => "/layouts/header" %>
+    <div id="wrapper">
+
+      <div id="flash-container">
+        <div class='container'>
+          <% flash.each do |name, message| -%>
+            <div class="flash" id="flash_<%= name %>">
+              <div class='container'>
+                <p><%= message %></p>
+              </div>
+            </div>
+          <% end -%>
+        </div>
+      </div>
+
+      <div id="content">
+        <%= yield %>
+      </div>
+      <%= render :partial => "/layouts/footer" %>
+    </div>
+  </body>
+</html>
+CODE
+
+file 'app/views/layouts/_header.erb', <<-CODE
+<div id="top">
+  <div class='logo'>
+    <%= link_to image_tag('logo.png'), root_path, :alt => 'logo' %>
+  </div>
+  <div class='actions'>
+    <ul>
+    <% if current_user %>
+      <li>Logged in as <strong><%= link_to current_user.login, current_user %></strong></li>
+      <li><%= link_to 'Settings', edit_account_url %></li>
+      <li><%= link_to 'Logout', logout_url %></li>
+    <% else %>
+      <li><%= link_to 'Register', register_url %></li>
+      <li><%= link_to 'Login', login_url %></li>
+    <% end %>
+    </ul>
+  </div>
+</div>
+
+CODE
+
+file 'app/views/layouts/_footer.erb', <<-CODE
+<p>footer</p>
 CODE
 
 file 'public/javascripts/application.js', <<-CODE
@@ -224,8 +295,8 @@ file 'app/views/user_sessions/new.html.erb', <<-CODE
 <br/>
 
 <% form_for @user_session, :url => user_session_path do |f| %>
-  <%= f.label :login, "Login" %>
-  <%= f.text_field :login %>
+  <%= f.label :email, "Email" %>
+  <%= f.text_field :email %>
   <%= clearfix %>
 
   <%= f.label :password %>
@@ -241,6 +312,62 @@ file 'app/views/user_sessions/new.html.erb', <<-CODE
 <% end %>
 CODE
 
+file 'public/stylesheets/reset.css', <<-CODE
+/* undohtml.css */
+/* (CC) 2004 Tantek Celik. Some Rights Reserved.                  */
+/* http://creativecommons.org/licenses/by/2.0                     */
+/* This style sheet is licensed under a Creative Commons License. */
+/* Purpose: undo some of the default styling of common (X)HTML browsers */
+
+/* link underlines tend to make hypertext less readable,
+   because underlines obscure the shapes of the lower halves of words */
+:link,:visited { text-decoration:none }
+
+/* no list-markers by default, since lists are used more often for semantics */
+ul,ol { list-style:none }
+
+/* avoid browser default inconsistent heading font-sizes */
+/* and pre/code too */
+h1,h2,h3,h4,h5,h6,pre,code { font-size:1em; }
+
+/* remove the inconsistent (among browsers) default ul,ol padding or margin  */
+/* the default spacing on headings does not match nor align with
+   normal interline spacing at all, so let's get rid of it. */
+/* zero out the spacing around pre, form, body, html, p, blockquote as well */
+/* form elements are oddly inconsistent, and not quite CSS emulatable. */
+/*  nonetheless strip their margin and padding as well */
+ul,ol,li,h1,h2,h3,h4,h5,h6,pre,form,body,html,p,blockquote,fieldset,input
+{ margin:0; padding:0 }
+
+/* whoever thought blue linked image borders were a good idea? */
+a img,:link img,:visited img { border:none }
+
+/* de-italicize address */
+address { font-style:normal }
+
+.clearfix {
+	clear: both;
+}
+CODE
+
+file 'public/stylesheets/sass/master.sass', <<-CODE
+body
+  :font-family "Helvetica Neue",Helvetica,Arial,sans-serif
+CODE
+
+file 'config/asset_packages.yml', <<-CODE
+---
+javascripts:
+- base:
+  - jquery
+  - jquery-form
+  - application
+stylesheets:
+- base:
+  - reset
+  - master
+CODE
+
 route 'map.root     :controller => "user_sessions", :action => "new"'
 route 'map.logout   "/logout",   :controller => "user_sessions", :action => "destroy"'
 route 'map.login    "/login",    :controller => "user_sessions", :action => "new"'
@@ -250,15 +377,9 @@ route 'map.root :controller => "user_sessions", :action => "new"'
 route 'map.resource :account, :controller => "users"'
 route 'map.resources :users'
 
-if yes?("Create and migrate databases now? (yes/no)")
-  rake("db:create:all")
-  rake("db:migrate")
-end
-
 puts "#" * 30
 puts "TO-DO checklist:"
 puts "\t* Write specs for the User model and controller"
-puts "\t* Create views for Authlogic - see http://github.com/binarylogic/authlogic_example/tree/master/app/views for examples"
 puts "\t* Generate your asset_packager config with: rake asset:packager:create_yml"
 puts "#" * 30
 
